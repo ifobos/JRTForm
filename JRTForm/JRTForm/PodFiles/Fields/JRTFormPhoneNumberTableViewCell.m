4135db63402d6eaabd6200da6efb07089b898a6a
Nelson Rivera <nelson.rivera@domandtom.com>
2016-06-14 09:43:31 -0600

    Optimizing and cleaning code

--- JRTForm/JRTForm/PodFiles/Fields/JRTFormPhoneNumberTableViewCell.m
+++ JRTForm/JRTForm/PodFiles/Fields/JRTFormPhoneNumberTableViewCell.m
@@ -24,6 +24,8 @@ NSString *const kJRTFormFieldPhoneNumberTableViewCell = @"JRTFormPhoneNumberTabl
     return _formatter;
 }
 
+
+
 - (IBAction)editingChangedAction:(UITextField *)textField {
     NSString *cleanedText = [[[[textField.text
                                 stringByReplacingOccurrencesOfString:@" " withString:@""]
@@ -31,80 +33,96 @@ NSString *const kJRTFormFieldPhoneNumberTableViewCell = @"JRTFormPhoneNumberTabl
                               stringByReplacingOccurrencesOfString:@"(" withString:@""]
                              stringByReplacingOccurrencesOfString:@")" withString:@""];
     
-    
     NSString * newText = [self.formatter inputString:cleanedText];
-    
     
     if (![textField.text isEqualToString:[newText stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]]) {
         
         UITextRange *textRange  = [textField.selectedTextRange copy];
         NSInteger startOffset   = [textField offsetFromPosition:textField.beginningOfDocument toPosition:textRange.start];
         NSInteger endOffset     = [textField offsetFromPosition:textField.beginningOfDocument toPosition:textRange.end];
-        
-        NSInteger formatCharactersInText = 0;
-        NSInteger formatCharactersInNewText = 0;
-        unichar findCharacter;
-        for (int i = 0; i < startOffset; i++) {
-            
-            if (i < textField.text.length) {
-                findCharacter = [textField.text characterAtIndex:i];
-                if (findCharacter == ' ' || findCharacter == '-' || findCharacter == '(' || findCharacter == ')') {
-                    formatCharactersInText ++;
-                }
-            }
-            
-            
-            if (i < newText.length) {
-                findCharacter = [newText characterAtIndex:i];
-                if (findCharacter == ' ' || findCharacter == '-' || findCharacter == '(' || findCharacter == ')') {
-                    formatCharactersInNewText ++;
-                }
-            }
-            
-        }
-        
-        NSInteger jumps = formatCharactersInNewText - formatCharactersInText;
-        NSInteger scope = startOffset + jumps;
-        
-        for (int i = startOffset; i < scope; i++) {
-            if (i < newText.length) {
-                findCharacter = [newText characterAtIndex:i];
-                if (findCharacter == ' ' || findCharacter == '-' || findCharacter == '(' || findCharacter == ')') {
-                    jumps ++;
-                }
-            }
-        }
-        
-        
-        
+        NSInteger jumps = [self iterateStartOffset:startOffset newText:newText inTextField:textField];
         
         startOffset += jumps;
         endOffset   += jumps;
         
         NSInteger totalLengDiference = newText.length - textField.tag;
         
-        
-        
-        while  ( startOffset < newText.length && startOffset > 0 && [cleanedText rangeOfString:[newText substringWithRange:NSMakeRange((startOffset<=0)? 0 : startOffset-1, 1)]].location == NSNotFound ) {
-            if (totalLengDiference > 0) {
-                startOffset++;
-                endOffset++;
-            }
-            else {
-                startOffset--;
-                endOffset--;
-            }
-            
-        }
+        NSDictionary *offsetDictionary = [self adjustOffsetNewText:newText cleanedText:cleanedText startOffset:startOffset endOffset:endOffset totalLenghtDifference:totalLengDiference];
         
         textField.text = newText;
-        UITextPosition *from    = [textField positionFromPosition:[textField beginningOfDocument] offset:startOffset];
-        UITextPosition *to      = [textField positionFromPosition:[textField beginningOfDocument] offset:endOffset];
+        UITextPosition *from    = [textField positionFromPosition:[textField beginningOfDocument] offset:((NSNumber *)offsetDictionary[@"startOffset"]).integerValue];
+        UITextPosition *to      = [textField positionFromPosition:[textField beginningOfDocument] offset:((NSNumber *)offsetDictionary[@"endOffset"]).integerValue];
         
         [textField setSelectedTextRange:[textField textRangeFromPosition:from toPosition:to]];
     }
     
     textField.tag = textField.text.length;
+}
+
+- (NSInteger)iterateStartOffset:(NSInteger)startOffset
+                        newText:(NSString *)newText
+                    inTextField:(UITextField *)textField {
+    
+    NSInteger formatCharactersInText = 0;
+    NSInteger formatCharactersInNewText = 0;
+    unichar findCharacter;
+    
+    for (NSInteger i = 0; i < startOffset; i++) {
+        
+        if (i < textField.text.length) {
+            findCharacter = [textField.text characterAtIndex:i];
+            if (findCharacter == ' ' || findCharacter == '-' || findCharacter == '(' || findCharacter == ')') {
+                formatCharactersInText ++;
+            }
+        }
+        
+        
+        if (i < newText.length) {
+            findCharacter = [newText characterAtIndex:i];
+            if (findCharacter == ' ' || findCharacter == '-' || findCharacter == '(' || findCharacter == ')') {
+                formatCharactersInNewText ++;
+            }
+        }
+        
+    }
+    
+    NSInteger jumps = formatCharactersInNewText - formatCharactersInText;
+    NSInteger scope = startOffset + jumps;
+    
+    for (NSInteger i = startOffset; i < scope; i++) {
+        if (i < newText.length) {
+            findCharacter = [newText characterAtIndex:i];
+            if (findCharacter == ' ' || findCharacter == '-' || findCharacter == '(' || findCharacter == ')') {
+                jumps ++;
+            }
+        }
+    }
+    
+    return jumps;
+}
+
+- (NSDictionary *)adjustOffsetNewText:(NSString *)newText
+                          cleanedText:(NSString *)cleanedText
+                          startOffset:(NSInteger)startOffset
+                            endOffset:(NSInteger)endOffset
+                totalLenghtDifference:(NSInteger)totalLenghtDifference{
+    
+    while  ( startOffset < newText.length && startOffset > 0 && [cleanedText rangeOfString:[newText substringWithRange:NSMakeRange((startOffset<=0)? 0 : startOffset-1, 1)]].location == NSNotFound ) {
+        if (totalLenghtDifference > 0) {
+            startOffset++;
+            endOffset++;
+        }
+        else {
+            startOffset--;
+            endOffset--;
+        }
+        
+    }
+    
+    return @{
+             @"startOffset": @(startOffset),
+             @"endOffset": @(endOffset)
+             };
 }
 
 @end
